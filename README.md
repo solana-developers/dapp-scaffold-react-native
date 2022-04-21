557d3114b6e62bb6dda0630b8d56ff435d48b19d
Bram Borggreve <borggreve@gmail.com>
2022-04-21 18:02:22 -0500

    feat: initial commit of React Native app

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# dapp-scaffold-react-native
-dApp scaffold for react native apps
+# DApp Scaffold React Native
+
+## Getting Started
+
+This is a [React Native](https://reactnative.dev) project based on the [React Native TypeScript template](https://github.com/react-native-community/react-native-template-typescript).
+
+## Installation
+
+```shell
+npm install
+# or
+yarn install
+```
+
+## Run the dev server (metro)
+
+```
+npm start
+# or 
+yarn start
+```
+
+## Run the Android app
+
+```
+npm run android
+# or 
+yarn android
+```
+
+## Run the iOS app
+
+```
+npm run ios
+# or 
+yarn ios
+```
+
+
---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native'
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen'
+import { Section } from './Section'
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
